6dc8798ca08933bb55900fd3dd1d0a41c45d05de
ArtyomZakharenko <artiomzaharenko@gmail.com>
2022-10-02 18:04:35 +0300

    added Landing page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,7 +1,31 @@
-function Landing () {
+import logo from '../assets/images/logo.svg';
+import main from '../assets/images/main.svg';
+import Wrapper from '../assets/wrappers/Landing';
+
+function Landing() {
 	return (
-		<div>
-			<h1>Landing</h1>
-		</div>
+		<Wrapper>
+			<main>
+				<nav>
+					<img src={logo} alt='jobster logo' className='logo'/>
+				</nav>
+				<div className='container page'>
+					{/* info */}
+					<div className='info'>
+						<h1>
+							job <span>tracking</span> app
+						</h1>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, ab aspernatur autem exercitationem facilis
+							perferendis perspiciatis quod rerum tempora voluptate.
+						</p>
+						<button className='btn btn-hero'>Login/Register</button>
+					</div>
+					<img src={main} alt='job hunt' className='img main-img'/>
+				</div>
+			</main>
+		</Wrapper>
 	);
 }
+
+export default Landing;
